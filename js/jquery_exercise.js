30215c869bde866add9779678e3121a2f4eff9ae
Jeffrey Neal <jeffneal11@gmail.com>
2023-01-13 15:32:24 -0600

    completed jquery2

---
+++ js/jquery_exercise.js
@@ -0,0 +1,5 @@
+// alert($("#list-item1").text());
+// $(".codeup").css("border", "solid 1px red");
+$("li").css("font-size", "20px");
+$("h1, li, p").css("background-color", "yellow");
+alert($("h1").text());
