a60b1f8406a1cc4a42ff63f0e71e4641f71f65a1
Jeffrey Neal <jeffneal11@gmail.com>
2023-01-30 12:47:45 -0600

    fixed github api caller

--- js/promises.js
+++ js/promises.js
@@ -1,6 +1,7 @@
 async function doCall() {
   const username = "toastbubbles";
-  const url = `https://api.github.com/repos/${username}/codeup-web-exercises/events`;
+  //const url = `https://api.github.com/repos/${username}/codeup-web-exercises/events`;
+  const url = `https://api.github.com/users/${username}/repos`;
   try {
     let resp = await fetch(url, {
       headers: { Authorization: `token ${keys.github}` },
@@ -8,20 +9,55 @@ async function doCall() {
     let data = await resp.json();
 
     // console.log(resp);
-    console.log(data[0]);
-    console.log(data[0].created_at);
-    console.log(data[0].payload.commits[0].message);
-    let date = data[0].created_at;
-    let message = data[0].payload.commits[0].message;
-    appender({ date, message });
+    console.log(data);
+    // console.log(data[0].created_at);
+    // console.log(data[0].payload.commits[0].message);
+    // let date = data[0].created_at;
+    // let message = data[0].payload.commits[0].message;
+    // appender({ date, message });
+    sortAndPrint(data);
   } catch (error) {
     console.log(error);
   }
 }
 doCall();
+
+function sortAndPrint(data) {
+  for (let i = 0; i < data.length; i++) {
+    // console.log(data[i].pushed_at);
+    // let arrayify = data[i].pushed_at.slice(0, -1).split("T");
+    // // console.log(arrayify);
+    // let year = arrayify[0].split("-")[0],
+    //   month = arrayify[0].split("-")[1],
+    //   day = arrayify[0].split("-")[2],
+    //   hour = arrayify[1].split(":")[0],
+    //   minute = arrayify[1].split(":")[1],
+    //   second = arrayify[1].split(":")[2];
+    // let dataObj = {
+    //   id: i + 1,
+    //   date: {
+    //     year,
+    //     month,
+    //     day,
+    //     hour,
+    //     minute,
+    //     second,
+    //   },
+    // };
+    // console.log(Date.parse(data[i].pushed_at));
+    // console.log(dataObj);
+  }
+  data.sort((a, b) => {
+    return Date.parse(b.pushed_at) - Date.parse(a.pushed_at);
+  });
+  //   console.log(data);
+  let name = data[0].name;
+  let date = data[0].pushed_at;
+  appender({ name, date });
+}
 function appender(obj) {
   console.log(obj);
   document.body.innerHTML += `<div>last commit:</div>`;
+  document.body.innerHTML += `<div>  name: ${obj.name}</div>`;
   document.body.innerHTML += `<div>  date: ${obj.date}</div>`;
-  document.body.innerHTML += `<div>  msg: ${obj.message}</div>`;
 }
